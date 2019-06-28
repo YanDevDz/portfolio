2b796a86f8f134a5bd208daee31a9efe756540b3
Abdelouahed <medjoudja@sindibadinternational.net>
2019-06-28 15:28:40 +0100

    change title

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
     <?php 
      require "public/header.global.html";
      ?>
 
-    <title>Me <3</title>
+    <title>Abdelouahed Medjoudja</title>
     <style type="text/css">
     	body {
     		color: white;
